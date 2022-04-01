3a948dc402f3954d0baa8393e308b7ad7fea74e5
JPWORLD <jaiprakashgupta@aol.com>
2022-04-01 17:26:36 +0530

    added columnDefs and row data dnd presented it on app-componennt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ag-grid-example';
+
+  columnDefs = [
+    {headerName: "Make", field:"make"},
+    {headerName: "Model", field:"model"},
+    {headerName: "Price", field:"price"},
+  ];
+  rowData = [
+    {make:'Audi', model:'A6',price:'232322'},
+    {make:'BMW', model:'Q5',price:'1223432'},
+    {make:'Ford', model:'eco',price:'243234'}
+  ];
 }
